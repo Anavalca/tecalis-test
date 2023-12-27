a08855bd73d627c5f8f126d49eb585d252722297
anavalca <avc92j@gmail.com>
2023-12-27 20:31:26 +0100

    second section

--- src/components/LaptopSection.js
+++ src/components/LaptopSection.js
@@ -1,9 +1,31 @@
-
+import laptop from '../images/computer.png'
 export const LaptopSection = () => {
 
   return (
-    <section className='laptopSection grid__desktop'> 
+    <section className='laptopSection grid__desktop'>
       <h2 className='heading__h2'>Lorem ipsum dolor sit amet</h2>
-    </section>
+      <div className='laptopSection__dropdown' >
+        <div className='laptopSection__dropdown__title active'>
+          <h4>Lorem ipsum</h4>
+          <span className='arrow-bottom'></span>
+        </div>
+        <div className='laptopSection__dropdown__info'>
+          <img src={laptop} className='body-1' alt='imagen ordenador' />
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed amet tortor diam habitasse tristique morbi tempus. Auctor semper diam dignissim turpis nisl ultricies tellus mi. Metus.</p>
+        </div>
+      </div>
+      <div className='laptopSection__dropdown' >
+        <div className='laptopSection__dropdown__title'>
+          <h4>Lorem ipsum</h4>
+          <span className='arrow-right'></span>
+        </div>
+      </div>
+      <div className='laptopSection__dropdown' >
+        <div className='laptopSection__dropdown__title'>
+          <h4>Lorem ipsum</h4>
+          <span className='arrow-right'></span>
+        </div>
+      </div>
+    </section >
   );
 }
--- src/components/MobileSection.js
+++ src/components/MobileSection.js
@@ -10,6 +10,7 @@ export const MobileSection = () => {
           Lorem ipsum dolor sit amet consectetur adipis
         </h1>
         <p className='body-1'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mollis adipiscing in turpis luctus ipsum, amet. Risus ac mauris pellentesque sed nunc nulla accumsan nec. Risus gravida donec tempor feugiat.</p>
+        <button className='App__button heading__h4'>Button</button>
       </div>
       <img src={mobile} class="mobileSection__img" alt="Imagen movil" />
     </section>
